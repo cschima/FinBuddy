099e44b8e06436700aeb5d8c1295cb2e90e0db53
ungjus <justinyong89@gmail.com>
2022-04-14 02:52:48 -0700

    Chat page now has calandly link buttons

--- app/src/main/java/com/example/fingrow/ui/chat/ChatFragment.kt
+++ app/src/main/java/com/example/fingrow/ui/chat/ChatFragment.kt
@@ -1,10 +1,16 @@
 package com.example.fingrow.ui.chat
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.fingrow.R
 import com.example.fingrow.databinding.FragmentChatBinding
 
 class ChatFragment : Fragment() {
@@ -32,6 +38,22 @@ class ChatFragment : Fragment() {
 //        chatViewModel.text.observe(viewLifecycleOwner) {
 //            textView.text = it
 //        }
+
+        binding.darrenMeeting.setOnClickListener{
+            val url = "https://calendly.com/ungjus19/fingrow-one-on-one-meeting?month=2022-04"
+            val i = Intent(Intent.ACTION_VIEW)
+            i.data = Uri.parse(url)
+            startActivity(i)
+        }
+
+        binding.sheliaMeeting.setOnClickListener{
+//            val url = "http://www.google.com"
+//            val i = Intent(Intent.ACTION_VIEW)
+//            i.data = Uri.parse(url)
+//            startActivity(i)
+            Toast.makeText(activity, "Meeting Link Not Available", Toast.LENGTH_SHORT).show();
+        }
+
         return root
     }
 
